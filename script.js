d319cb84b28d83f665fd64d3e6bc12b59a2b1597
kristiansnts <epafroditus.kritian@gmail.com>
2023-08-08 22:16:00 +0700

    add js code for the game

--- script.js
+++ script.js
@@ -7,3 +7,31 @@
 // Get Player Choice
 // - make prompt
 // Game Set
+
+function getComputerChoice() {
+    const choice = ['rock', 'paper', 'scissors'];
+    const random = Math.floor(Math.random()*3);
+    return choice[random];
+}
+
+function getPlayerChoice() {
+    const player = prompt("write your input: rock/paper/scissors").toLowerCase();
+    return player;
+}
+
+function playRound() {
+    const computer = getComputerChoice();
+    const player = getPlayerChoice();
+
+    if(player == computer) {
+        return 'DRAW'
+    } else if (computer == 'rock' && player == 'paper') {
+        return 'WIN'
+    } else if (computer == 'paper' && player == 'scissors') {
+        return 'WIN'
+    } else if (computer == 'scissors' && player == 'rock') {
+        return 'WIN'
+    } else {
+        return 'LOSE'
+    }
+}
