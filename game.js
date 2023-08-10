07b657776be14081419e10c3e1c87ec030b148fc
kristiansnts <epafroditus.kritian@gmail.com>
2023-08-10 17:42:26 +0700

    add script to change content

---
+++ game.js
@@ -0,0 +1,41 @@
+function getComputerChoice() {
+    const choice = ['rock', 'paper', 'scissors'];
+    const random = Math.floor(Math.random()*3);
+    return choice[random];
+}
+
+function getPlayerChoice() {
+    const player = prompt("write your input: rock/paper/scissors").toLowerCase();
+    return player;
+}
+
+function playRound() {
+    const computer = getComputerChoice();
+    const player = getPlayerChoice();
+
+    if(player == computer) {
+        return 'DRAW'
+    } else if (computer == 'rock' && player == 'paper') {
+        return 'WIN'
+    } else if (computer == 'paper' && player == 'scissors') {
+        return 'WIN'
+    } else if (computer == 'scissors' && player == 'rock') {
+        return 'WIN'
+    } else {
+        return 'LOSE'
+    }
+}
+
+// let n = 5;
+
+let point = 0;
+function game() {
+    for(let i = 1; i <= n; i++) {
+        result = playRound();
+        confirm(`${result}`)
+        if(result == 'WIN') {
+            point += 1
+        }
+    }
+    confirm(`You WIN ${point} times`);
+}
--- script.js
+++ script.js
@@ -1,41 +1,15 @@
-function getComputerChoice() {
-    const choice = ['rock', 'paper', 'scissors'];
-    const random = Math.floor(Math.random()*3);
-    return choice[random];
-}
+function fetchHtml() {
+    fetch('game.html')
+    .then((response) => {
+      return response.text();
+    })
+    .then((html) => {
+      document.body.innerHTML = html;
+      document.body.style.backgroundColor = "white";    
+    });
+  }
 
-function getPlayerChoice() {
-    const player = prompt("write your input: rock/paper/scissors").toLowerCase();
-    return player;
-}
-
-function playRound() {
-    const computer = getComputerChoice();
-    const player = getPlayerChoice();
-
-    if(player == computer) {
-        return 'DRAW'
-    } else if (computer == 'rock' && player == 'paper') {
-        return 'WIN'
-    } else if (computer == 'paper' && player == 'scissors') {
-        return 'WIN'
-    } else if (computer == 'scissors' && player == 'rock') {
-        return 'WIN'
-    } else {
-        return 'LOSE'
-    }
-}
-
-let point = 0;
-function game() {
-    for(let i = 1; i <= n; i++) {
-        result = playRound();
-        confirm(`${result}`)
-        if(result == 'WIN') {
-            point += 1
-        }
-    }
-    confirm(`You WIN ${point} times`);
-}
-
-game();
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(button => button.addEventListener('click', () => {
+    fetchHtml();
+}));
