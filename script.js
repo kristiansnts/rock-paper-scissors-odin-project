f75472a6c3bc3d392ab614abedaaa0abfd91c4fb
kristiansnts <epafroditus.kritian@gmail.com>
2023-08-10 16:30:25 +0700

    add homepage UI

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// Pseudocode
-
-// Input
-// Get Computer Choice
-// - set array
-// - set random number from 1 - 3
-// Get Player Choice
-// - make prompt
-// Game Set
-
-const user = prompt("Tell your name buddy!");
-confirm(`Welcome to Rock-Paper-Scissors Game, ${user} \n 
-Please follow the rule for playing this game. \n
-Rules: choose your power (rock/paper/scissor). \n
-ROCK > SCISSORS > PAPER > ROCK. \n
-READY For the Game?
-`);
-
-const n = parseInt(prompt('How many times you will play?'));
-
 function getComputerChoice() {
     const choice = ['rock', 'paper', 'scissors'];
     const random = Math.floor(Math.random()*3);
